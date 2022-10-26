c960e8172f778253c6530ebcfd1701dfb9941bd2
lambertkevin <kevin.lambert@ledger.fr>
2022-10-26 17:14:42 +0200

    Update dep path in test file

--- libs/ledger-live-common/src/families/ethereum/__tests__/hw-signMessage.test.ts
+++ libs/ledger-live-common/src/families/ethereum/__tests__/hw-signMessage.test.ts
@@ -1,4 +1,4 @@
-import "../../__tests__/test-helpers/setup";
+import "../../../__tests__/test-helpers/setup";
 
 import { EIP712Message } from "@ledgerhq/hw-app-eth/lib/modules/EIP712";
 import testEIP712Message from "@ledgerhq/hw-app-eth/tests/sample-messages/0.json";
